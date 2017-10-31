f388aad74db92c581d7cc63f2edb2380b15d5429
aldominasi <aldo.minasi@gmail.com>
2017-10-31 16:28:40 +0100

    Ho terminato l'aggiunta di una chiavetta

--- routes/aggiungi.js
+++ routes/aggiungi.js
@@ -1,21 +1,36 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('shelljs');
+var fs = require('fs');
+var shell = require('shelljs');
+var sudo = require('sudo');
+
+var options = {
+    cachePassword: true
+};
 
 router.get('/', function (req, res) {
     res.render('aggiungi');
 });
 
 router.post('/', function (req, res) {
-    /* var path = "/etc/";
-     fs.readdir(path, function (err, items) {
-      console.log(items);
-         for (var i=0; i < items.length; i++){
-             console.log(items[i]);
-         }
-     })*/
-   var items = fs.pwd();
-   console.log(items);
+    var count = 0;
+    shell.cd("/etc");
+    var items = shell.ls();
+    for (var i=0;i<items.length;i++){
+        if (/gammu-smsdrc-./.test(items[i]))
+            count++;
+    }
+    count += 1;
+    shell.exec("echo 0000 | sudo -S touch /etc/gammu-smsdrc-" + count);
+    shell.exec("echo 0000 | sudo -S chmod 777 /etc/gammu-smsdrc-" + count);
+    shell.exec("cat /etc/gammu-smsdrc-1 >> /etc/gammu-smsdrc-" + count);
+    shell.sed('-i',/\/dev\/gsmmodem/,'/dev/gsmmodem' + count,'/etc/gammu-smsdrc-' + count);
+    shell.sed('-i', /GSM[0-9]*/, 'GSM' + count,'/etc/gammu-smsdrc-' + count);
+    shell.sed('-i', /phoneid = [0-9]*/, 'phoneid = ' + req.body['phoneid'], '/etc/gammu-smsdrc-' + count);
+    var pattern = shell.grep(/\$config\['multiple_modem'\]*/, '/home/aldo/Scrivania/kalkun_settings.php');
+    var newPattern = pattern.replace(');\n', '');
+    newPattern = newPattern.concat(",'"+req.body['phoneid']+"');");
+    shell.sed('-i',/^\$config\['multiple_modem'\] = array.*/,newPattern,'/home/aldo/Scrivania/kalkun_settings.php');
 });
 
 module.exports = router;
