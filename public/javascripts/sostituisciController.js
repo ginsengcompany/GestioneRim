45161080a32d14e0b0d42bbad88dea70dc461a36
Alessio Calabrese <darkalex21@gmail.com>
2017-11-02 14:35:01 +0100

    Aggiunto commenti e inclusi file locali di bootstrap

--- public/javascripts/sostituisciController.js
+++ public/javascripts/sostituisciController.js
@@ -1,16 +1,19 @@
+/**
+ * Function utilizzata Per sostituire una  chiavetta
+ */
 var sostituisciImei = function () {
-    var oldImei = document.getElementById('selectImei').value;
-    var newImei = document.getElementById('inputNewImei').value;
-    if (/.*[0-9].*/.test(newImei) && newImei.trim().length === 15){
-        $.post("http://localhost:3000/sostituisci",{"oldImeiBody": oldImei, "newImeiBody": newImei}, function (response) {
-            if(response.status === 200){
+    var oldImei = document.getElementById('selectImei').value; //Recuperiamo vecchio imei scelto dalla select
+    var newImei = document.getElementById('inputNewImei').value; //Recuperiamo nuovo imei scritto
+    if (/.*[0-9].*/.test(newImei) && newImei.trim().length === 15){ //Controllo del campo inputNewImei
+        $.post("http://localhost:3000/sostituisci",{"oldImeiBody": oldImei, "newImeiBody": newImei}, function (response) { //Risposta della post
+            if(response.status === 200){ //Se la post è andata a buon fine mosta un alert verde
                 document.getElementById('alertSostituzione').innerHTML = response.message;
                 document.getElementById('alertSostituzione').style.display = 'block';
                 document.getElementById('alertSostituzione').className = "alert alert-success";
             }
         });
     }
-    else {
+    else { //Errore nella post viene mostrato un alert rosso
         document.getElementById('alertSostituzione').className = "alert alert-danger";
         document.getElementById('alertSostituzione').innerHTML = "Controllare i campi inseriti";
         document.getElementById('alertSostituzione').style.display = 'block';
--- routes/sostituisci.js
+++ routes/sostituisci.js
@@ -3,28 +3,25 @@ var router = express.Router();
 var mysql = require('../configDatabase/dbConfig');
 
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    var connection = mysql.getMySQLConnection();
+router.get('/', function(req, res, next) { //Rotta di accesso alla pagina sostituisci.jade
+    var connection = mysql.getMySQLConnection(); //Recupera la connessione al database
     connection.query("SELECT IMEI FROM a_sim WHERE IMEI <> 'SMS1' AND IMEI <> 'SMS2' AND IMEI <> 'SMS3' AND IMEI <> 'SMS4'", function (err, rows, fields) {
-        if (err)
+        if (err)//Se la query non è andata a buon fine lancia l'errore
           throw err;
-        res.render('sostituisci', {oldImei: rows});
+        res.render('sostituisci', {oldImei: rows}); //Renderizza la pagina e mostra in una select gli imei già presenti nel database
     });
 });
-router.post('/', function (req, res){
-    var newImei = req.body.newImeiBody;
-    var oldImei = req.body.oldImeiBody;
-    console.log(newImei);
-    console.log(oldImei);
-    var connection = mysql.getMySQLConnection();
+router.post('/', function (req, res){ //Rotta per il servizio di sostituzione della nuova chiavetta
+    var newImei = req.body.newImeiBody; //Variabile per memorizzare imei della nuova chiavetta
+    var oldImei = req.body.oldImeiBody; // Variabile per memorizza imei della vecchia chiavetta
+    var connection = mysql.getMySQLConnection();//Recupera la connessione al database
     var sql = "UPDATE a_sim SET IMEI = '" + newImei + "' WHERE IMEI = '" + oldImei + "'";
     connection.query(sql, function (err, rows, fields) {
-        if (err){
+        if (err){ //Se la query non è andata a buon fine lancia l'errore
           res.json({'status': 500, 'message': "Errore nell'inserimento"});
         }
         else{
-          res.json({'status': 200, 'message': "Inserimento avvenuto con successo"});
+          res.json({'status': 200, 'message': "Inserimento avvenuto con successo"}); //La query è anadata a buon fine restituendo un alert
         }
     })
 });
